995ed357a0c8679a3ed7c38ea9cb56c7aeafed45
tienshunlo <tienshunlo@gmail.com>
2017-06-16 05:55:50 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+create_doctors = for i in 1..20 do
+    Doctor.create!([name: "Doctor no.#{i}", specialty: "這是用種子建立的第 #{i} 個專長", experience: "這是用種子建立的第 #{i} 個經驗", hospital_id:: i, gender: 0, city_id: i, district_id: i])
+    City.create!([name: "City no.#{i}"])
+    District.create!([name: " District no.#{i}", city_id:i])
+    Hospital.create!([name: " Hospital no.#{i}", city_id:i, district_id:i])
+    Phase.create!([title: " Phase no.#{i}"])
+    Issue.create!([title: " Issue no.#{i}", phase_id:i, district_id:i])
+end
+
+
