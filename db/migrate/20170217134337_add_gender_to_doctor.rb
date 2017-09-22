6bcd2e17359e82067ee53a2051ddd10fac80b39f
tienshunlo <tienshunlo@gmail.com>
2017-09-22 03:30:32 +0000

    new beginning

--- db/migrate/20170217134337_add_gender_to_doctor.rb
+++
@@ -1,5 +0,0 @@
-class AddGenderToDoctor < ActiveRecord::Migration
-  def change
-    add_column :doctors, :gender, :integer, :limit => 1 , :default => 0 , :null => false , :unsigned => true
-  end
-end
