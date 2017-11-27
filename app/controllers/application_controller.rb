fd6b6db56f109c471590c5ac578a621fc4d89bfc
tienshunlo <tienshunlo@gmail.com>
2017-11-27 06:39:26 +0000

    Added Controller Concern for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include DefaultPageContent
-  
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end
+  include DeviseWhitelist
 
 end
---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,9 @@
+module DeviseWhitelist
+    extend ActiveSupport::Concern
+    included do
+        before_filter :configure_permitted_parameters, if: :devise_controller?
+    end
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+end
