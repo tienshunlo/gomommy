8ad0825c5d3228d120320913c82c5f97cf049f4a
tienshunlo <tienshunlo@gmail.com>
2017-12-14 04:58:49 +0000

    albums problem primary key Mysql2::Error: Can't create table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214043352) do
+ActiveRecord::Schema.define(version: 20171214045614) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name",       limit: 255
