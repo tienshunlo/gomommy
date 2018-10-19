40fc8fb8c6321a731ec9dc9214a9cc78db63d644
tienshunlo <tienshunlo@gmail.com>
2018-10-19 06:40:09 +0000

    edit create.js.erb add error message

--- app/controllers/dashboard/messages_controller.rb
+++ app/controllers/dashboard/messages_controller.rb
@@ -9,27 +9,9 @@ class Dashboard::MessagesController < Dashboard::DashboardController
         
       respond_to do |format|
         if @message.save
-          #format.html { redirect_to dashboard_conversation_path(@conversation) }
           format.js   { }
-          #format.json { 
-          #  render json:{
-          #    html_data: render_to_string(partial: 'dashboard/messages/message', :formats => [:html], locals: {message: @message})
-          #  }
-          #}
-          #render json: {
-            #html_data: render_to_string(partial: 'dashboard/messages/message', locals: { message: @message })
-          #} //可用，但不必有respond_to do |format|
-          #format.json { render json: @message.to_json, status: :ok } //資料有回傳，但不知道怎麼放到html裡面
-          #format.json { 
-            #render json: {:message => @message, :template => "dashboard/messages/message", status: :ok}
-          #} //資料有回傳，但不知道怎麼放到html裡面
         else
-          format.json { render json: @message.errors, status: :unprocessable_entity }
-          #format.json { 
-            #render json:{ 
-              #:errors => @message.errors.as_json, :status => :unprocessable_entity 
-            #}
-          #}
+          format.js
         end
       end
 	end
